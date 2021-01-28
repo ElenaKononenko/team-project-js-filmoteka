5ecadb182826d373779520ff1765c15c05185cb0
Tetiana-Lykhovei <tlykhovei@gmail.com>
2021-01-28 23:01:53 +0200

    /

--- src/js/4filmDetailsPage.js
+++ src/js/4filmDetailsPage.js
@@ -1,40 +1,40 @@
 console.log('Hello from 4filmDetailsPage');
 
-function onPicClick(event) {
-  event.preventDefault();
-  // console.log(event.target.dataset);
-  // console.log(event.target.nodeName);
+// function onPicClick(event) {
+//   event.preventDefault();
+//   // console.log(event.target.dataset);
+//   // console.log(event.target.nodeName);
 
-  if (event.target.nodeName !== 'IMG') {
-    console.log('мимо');
-    return;
-  } else {
-    console.log(event.target.dataset.source);
-  }
+//   if (event.target.nodeName !== 'IMG') {
+//     console.log('мимо');
+//     return;
+//   } else {
+//     console.log(event.target.dataset.source);
+//   }
 
-  backDropRef.classList.add('is-open');
-  lightboxImg.src = event.target.dataset.source;
-  window.addEventListener('keydown', onPressEscape);
-}
+//   backDropRef.classList.add('is-open');
+//   lightboxImg.src = event.target.dataset.source;
+//   window.addEventListener('keydown', onPressEscape);
+// }
 
-closeModalBtn.addEventListener('click', onCloseModal);
-OverlayRef.addEventListener('click', onBackDropClick);
+// closeModalBtn.addEventListener('click', onCloseModal);
+// OverlayRef.addEventListener('click', onBackDropClick);
 
-function onCloseModal() {
-  window.removeEventListener('keydown', onPressEscape);
-  backDropRef.classList.remove('is-open');
-  lightboxImg.src = '';
-}
+// function onCloseModal() {
+//   window.removeEventListener('keydown', onPressEscape);
+//   backDropRef.classList.remove('is-open');
+//   lightboxImg.src = '';
+// }
 
-function onBackDropClick(event) {
-  if (event.target === event.currentTarget) {
-    onCloseModal();
-  }
-  lightboxImg.src = '';
-}
+// function onBackDropClick(event) {
+//   if (event.target === event.currentTarget) {
+//     onCloseModal();
+//   }
+//   lightboxImg.src = '';
+// }
 
-function onPressEscape(event) {
-  if (event.code === 'Escape') {
-    onCloseModal();
-  }
-}
+// function onPressEscape(event) {
+//   if (event.code === 'Escape') {
+//     onCloseModal();
+//   }
+// }
