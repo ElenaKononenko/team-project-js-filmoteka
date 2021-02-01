0268a52e6449ace54a1d02edf778aaa0493b4047
Tetiana-Lykhovei <tlykhovei@gmail.com>
2021-02-01 18:29:37 +0200

    игнорировать этот коммит

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -1,24 +1,27 @@
 console.log('Hello from 3navigation');
+const modalCard = document.querySelector('.modalCard');
 const backDropRef = document.querySelector('.js-modal');
 const overlayRef = document.querySelector('.overlay');
 
-showDetails(movieId);
-fetchPopularMoviesListModal();
+// listItem.addEventListener('click', activeDetailsPage);
 
-function showDetails(
-  imgPath,
-  filmTitle,
-  voteAverage,
-  voteCount,
-  popularity,
-  originalTitle,
-  genre,
-  description,
-  id,
-) {
-  const modalWindow = document.createElement('div');
-  modalWindow.classList.add('modalCard is-hidden');
+function activeDetailsPage(movie) {
+  window.addEventListener('keydown', onPressEscape);
+  overlayRef.addEventListener('click', onBackDropClick);
+  backDropRef.classList.add('is-open');
+  showDetails(movie);
+}
 
+function showDetails({
+  poster_path: imgPath,
+  title: filmTitle,
+  vote_average: voteAverage,
+  vote_count: voteCount,
+  popularity: popularity,
+  original_title: originalTitle,
+  genre_ids: genre,
+  overview: description,
+}) {
   const modalCardinfo = `<img class="gallery__item__picture"
                     src='${basicPosterUrl}${imgPath}'
                     alt=${filmTitle}
@@ -27,66 +30,23 @@ function showDetails(
         <p class="votes">Vote/Votes${voteAverage}${voteCount}</p>
         <p class="votes">Popularity${popularity}</p>
         <p class="votes">Original Title${originalTitle}</p>
-        <p class="votes">${genreString}</p>
+        <p class="votes">${genreString(genre)}</p>
         <h2 class="about">ABOUT</h2>
         <p class="votes about">${description}</p>`;
 
-  modalWindow.insertAdjacentHTML('afterbegin', modalCardinfo);
+  modalCard.insertAdjacentHTML('afterbegin', modalCardinfo);
 }
-
-function fetchPopularMoviesListModal() {
-  let url = `https://api.themoviedb.org/3/trending/${MEDIA_TYPE}/${TIME_WINDOW}?api_key=${API_KEY}&page=${pageNumber}`;
-
-  return fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      renderFilms = data.results;
-      list.innerHTML = '';
-      const cardsFragment = document.createDocumentFragment();
-
-      renderFilms.map(el => {
-        cardsFragment.appendChild(
-          createCardFunc(
-            el.poster_path,
-            el.title,
-            el.vote_average,
-            el.vote_count,
-            el.popularity,
-            el.original_title,
-            el.genre_ids,
-            el.overview,
-            el.id,
-          ),
-        );
-      });
-      list.appendChild(cardsFragment);
-    })
-    .catch(Error => {
-      console.log(Error);
-    });
-}
-
-function activeDetailsPage(event) {
-  event.preventDefault();
-}
-
-backDropRef.classList.add('is-open');
-gallery.classList.add('is-hidded');
-window.addEventListener('keydown', onPressEscape);
-
-OverlayRef.addEventListener('click', onBackDropClick);
 
 function onCloseModal() {
   window.removeEventListener('keydown', onPressEscape);
   backDropRef.classList.remove('is-open');
-  gallery.classList.remove('is-hidded');
+  modalCard.innerHTML = '';
 }
 
 function onBackDropClick(event) {
   if (event.target === event.currentTarget) {
     onCloseModal();
   }
-  gallery.classList.add('is-hidded');
 }
 
 function onPressEscape(event) {
--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -1,22 +1,33 @@
+// console.log('Hello page5');
+// const listLibrary = document.querySelector('.galleryLibrary');
+// function createLibraryCardFunc(
+//   imgPath,
+//   filmTitle,
+//   genre,
+//   date,
+//   voteAverage,
+//   movieId,
+// ) {
+//   const listLibraryItem = cardTemplate(
+//     imgPath,
+//     filmTitle,
+//     genre,
+//     date,
+//     voteAverage,
+//   );
+//   listLibraryItem.addEventListener('click', () => {
+//     activeDetailsPage(movieId, true);
+//   });
+//   return listLibraryItem;
+// }
+
 console.log('Hello page5');
 const listLibrary = document.querySelector('.galleryLibrary');
-function createLibraryCardFunc(
-  imgPath,
-  filmTitle,
-  genre,
-  date,
-  voteAverage,
-  movieId,
-) {
-  const listLibraryItem = cardTemplate(
-    imgPath,
-    filmTitle,
-    genre,
-    date,
-    voteAverage,
-  );
+
+function createLibraryCardFunc(movie) {
+  const listLibraryItem = cardTemplate(movie);
   listLibraryItem.addEventListener('click', () => {
-    activeDetailsPage(movieId, true);
+    activeDetailsPage(movie);
   });
   return listLibraryItem;
 }
