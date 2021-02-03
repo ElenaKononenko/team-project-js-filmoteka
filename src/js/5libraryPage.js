abb064e0f27b849610a6ac654a76f868ac943301
MaksimLisovoi <lisovoi123@gmail.com>
2021-02-03 20:40:01 +0200

    Менет Хедер по нажатию на myLibrary

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -73,3 +73,22 @@ function readQueueLocalStorage() {
   const parsedSettings = JSON.parse(savedSettings);
   parsedSettings.map(e => console.log(e));
 }
+
+// Менет Хедер по нажатию на myLibrary
+
+const homeHeader =  document.getElementById('homeHeader');
+const libaryHeader = document.getElementById('libraryHeader');
+const libraryLink = document.getElementById('libraryLink');
+const homeLink = document.getElementById('homeLink');
+
+
+
+libraryLink.addEventListener('click', (e) => {
+  console.log(e.target);
+  homeHeader.classList.add('visually-hidden')
+  libaryHeader.classList.remove('visually-hidden')
+})
+
+homeLink.addEventListener('click', e => {
+  homeHeader.classList.remove('visually-hidden')
+})
